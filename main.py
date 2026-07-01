a4afa3a1f9a779f6e3ca9af82dd6430a627612ec
roxxxs-cpu <vasiyasyed89@gmail.com>
2026-07-02 01:30:04 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 import speech_recognition as sr
 import webbrowser
 import pyttsx3
+import songs
+import spotipy
 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+clientid = os.getenv("clientid")
+clientsecret = os.getenv("clientsecret")
+clienturi = os.getenv("clienturi")
+
+from spotipy.oauth2 import SpotifyOAuth
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        client_id=clientid,
+        client_secret=clientsecret,
+        redirect_uri=clienturi,
+        scope="user-read-playback-state user-modify-playback-state"
+    )
+)
 
 recognizer = sr.Recognizer()
 def speak(text):
@@ -10,6 +30,85 @@ def speak(text):
     engine.runAndWait()
     engine.stop()
 # speak("Hello,Sir! am Jarvis, your personal assistant. How can I help you today?")
+def processcommand():
+    if "open youtube" in command.lower():
+        speak("Opening YouTube")
+        webbrowser.open("https://www.youtube.com")
+
+    elif "open google" in command.lower():
+        speak("Opening Google")
+        webbrowser.open("https://www.google.com")
+
+    elif "open facebook" in command.lower():
+        speak("Opening Facebook")
+        webbrowser.open("https://www.facebook.com")
+
+    elif "open instagram" in command.lower():
+        speak("Opening Instagram")
+        webbrowser.open("https://www.instagram.com")
+
+    elif "who is gay" in command.lower():
+        speak("Raina is")
+
+    elif "how are you" in command.lower():
+        speak("I am fine, Sir. How are you?")
+
+    elif "fine" in command.lower():
+        speak("That's great to hear, Sir.")
+
+    elif "play" in command.lower():
+
+        song_name = command.lower().replace("play", "").strip()
+
+        try:
+            result = sp.search(
+                q=song_name,
+                type="track",
+                limit=2)
+            
+
+            track = result["tracks"]["items"][0]
+
+            uri = track["uri"]
+
+            devices = sp.devices()["devices"]
+
+            if not devices:
+                speak("No active Spotify device found.")
+                return
+
+            device_id = devices[0]["id"]
+
+            sp.start_playback(
+                device_id=device_id,
+                uris=[uri]
+            )
+
+            speak(f"Playing {track['name']} by {track['artists'][0]['name']}")
+
+        except Exception as e:
+            print(e)
+            speak("Sorry Sir, I couldn't play that song.")
+
+    elif "pause" in command.lower():
+        sp.pause_playback()
+        speak("Music paused.")
+
+    elif "resume" in command.lower():
+        sp.start_playback()
+        speak("Resuming music.")
+
+    elif "next" in command.lower():
+        sp.next_track()
+        speak("Skipping.")
+
+    elif "previous" in command.lower():
+        sp.previous_track()
+        speak("Going back.")
+    
+    elif "exit" in command.lower():
+        speak("   Goodbye, Sir. Have a great day!")
+        exit()
 
 if __name__ == "__main__":
     speak("Hello,Sir! am Jarvis, your personal assistant. How can I help you today?")
@@ -33,31 +132,10 @@ if __name__ == "__main__":
                             print("Recognizing...")
                             command = recognizer.recognize_google(audio)
                             print(f"{command}")
-                            if "open youtube" in command.lower():
-                                speak("Opening YouTube")
-                                webbrowser.open("https://www.youtube.com")
-                            elif "open google" in command.lower():
-                                speak("Opening Google")
-                                webbrowser.open("https://www.google.com")
-                            elif "open facebook" in command.lower():
-                                speak("Opening Facebook")
-                                webbrowser.open("https://www.facebook.com")
-                            elif "open instagram" in command.lower():
-                                speak("Opening Instagram")
-                                webbrowser.open("https://www.instagram.com")
-                            elif "who is gay" in command.lower():
-                                speak("Raina is")
-                            elif "how are you" in command.lower():
-                                speak("I am fine, Sir. How are you?")
-                            elif "fine" in command.lower():
-                                speak("That's great to hear, Sir.")
-                            elif "exit" in command.lower():
-                                speak("Goodbye, Sir. Have a great day!")
-                                exit()
+                            processcommand()
                     except Exception as e:
                         print("Error:", str(e))
         except Exception as e:
             print("Error:", str(e))
-            speak("Sorry, I didn't catch that. Please try again.")
 
 
---
+++ songs.py
@@ -0,0 +1,5 @@
+song = {
+    "apocalypse": "https://open.spotify.com/track/1oAwsWBovWRIp7qLMGPIet?si=7692de3cb685406b",
+    "here with me" :"https://open.spotify.com/track/0NLm9bQG7ikL5k9x9TtYT7?si=d24d8e314f914521",
+    "let down" : "https://open.spotify.com/track/2fuYa3Lx06QQJAm0MjztKr?si=5da4481fc3014d0d"
+}
